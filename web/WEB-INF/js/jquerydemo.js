f0df2df78ba317eb44b5ce52de1677317090bb88
meganathan <mega91raja@gmail.com>
2021-09-15 07:14:43 +0530

    practiced about jquery effect, file name DemoJquery.html.

--- web/WEB-INF/js/jquerydemo.js
+++ web/WEB-INF/js/jquerydemo.js
@@ -1,5 +1,5 @@
 $("document").ready(function(){
-
+        var x = 0;
         $("#btn").click(function(){
             $("#h").hide();
         });
@@ -56,4 +56,89 @@ $("document").ready(function(){
              function(){
            $(this).css("background-color","pink");
         });
+
+        $("#mouse").mousedown(function(){
+            $("p").text("Mouse down Event");
+        });
+
+        $("#mouse").mouseup(function(){
+            $("p").text("Mouse up Event");
+        });
+
+        $("#mouse").mouseenter(function(){
+            $("p").text("Mouse enter Event");  // mouseover
+        });
+
+        $("#mouse").mouseleave(function(){     // mouseout
+            $("p").text("Mouse leave Event");
+        });
+
+        /* key event */
+
+        $("#fname").keydown(function(){
+            console.log("keydown");
+        });
+
+        $("#fname").keyup(function(){
+            console.log("keyup");
+        });
+
+        $("#lname").keypress(function(){
+            $("span").text(x += 1);
+       });
+
+          /* jquery effects */
+
+       $("#btn").click(function(){
+            $("#effect").show();
+       });
+
+      $("#btn").click(function(){
+            $("img").toggle();
+      });
+
+     $("#btn").click(function(){
+           $("#div_1").fadeIn();
+           $("#div_2").fadeIn("slow");      // here replace fadeIn by toggle and see the result
+           $("#div_3").fadeIn(3000);
+     });
+
+    $("#btn").click(function(){
+           $("#div_4").fadeOut();
+           $("#div_5").fadeOut("slow");
+           $("#div_6").fadeOut(3000);
+     });
+
+    $("#btn").click(function(){
+           $("#div_7").fadeTo("slow",0.5);
+           $("#div_8").fadeTo("slow",0.3);
+           $("#div_9").fadeTo("slow",0.8);
+    });
+
+        /* jquery slide effect */
+
+    $("#div_10").click(function(){
+        $("#div_11").slideDown(3000);
+    });
+
+        /* jquery slide stop effect */
+
+    $("#stop_effect").click(function(){
+        $("#div_11").stop();
+    });
+
+            /* jquery callback effect */
+
+    $("#calbk-eft").click(function(){
+        $("#h_1").toggle(3000,function(){
+            alert("callback effect");
+        });
+    });
+
+                /* jquery chaining effect */
+
+    $("#calbk-eft").click(function(){
+            $("#h_1").css("color","red").slideUp(1000).slideDown(2000);
+    });
+
 });
