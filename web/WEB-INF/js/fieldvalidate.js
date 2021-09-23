44c2a435c5cc52b1ceda9586f30f588ca920309c
meganathan <mega91raja@gmail.com>
2021-09-23 09:48:12 +0530

    designed signup and login page with various fields, validated signup page using jQuery.: c

---
+++ web/WEB-INF/js/fieldvalidate.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    $.validator.setDefaults({
+        submitHandler: function(form) {
+            form.submit();
+        }
+      });
+   $("#signup").validate({
+      rules: {
+         firstname: "required",
+         lastname: "required",
+         email: {
+             required: true,
+             email: true,
+             maxlength: 255
+         },
+         password: {
+             required: true,
+             minlength: 5,
+             maxlength: 12
+         },
+         conformPassword: {
+             required: true,
+             minlength: 5,
+             maxlength: 12,
+             equalTo: '[name=password]'
+         },
+         phone: {
+             required: true,
+             number: true,
+             minlength: 10,
+             maxlength: 10
+         },
+         gender: {
+             required: true
+         },
+         birthdate: {
+            required: true,
+            date: true
+         }
+      },
+      messages: {
+            firstname: "Please enter first name",
+            lastname: "Please enter last name",
+            email: "Please enter Email",
+            password: "Enter Password",
+            conformPassword: "Enter Confirm Password Same as Password",
+            phone: "Enter 10 digits mobile no",
+            gender: "Select Gender",
+            birthdate: {
+                required: "Enter your DOB",
+                date: "Please enter correct date format"
+            }
+      }
+
+   });
+
+});
