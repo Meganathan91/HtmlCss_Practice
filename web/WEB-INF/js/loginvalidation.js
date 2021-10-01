91d2bbb1b7f04f7ceb7ea31abeaf6c5520974195
meganathan <mega91raja@gmail.com>
2021-10-01 11:29:09 +0530

    Signup and Login form validation completed

--- web/WEB-INF/js/loginvalidation.js
+++ web/WEB-INF/js/loginvalidation.js
@@ -1,87 +1,38 @@
-$(document).ready(function() {
-
+$(document).ready(function(){
         $("#eye").click(function() {
-           $(this).toggleClass("fa-eye fa-eye-slash");
-           var type = $(this).hasClass("fa-eye") ? "text" : "password";
-           $("#pass").attr("type", type);
+            $(this).toggleClass("fa-eye fa-eye-slash");
+            var type = $(this).hasClass("fa-eye") ? "text" : "password";
+            $("#pass").attr("type",type);
         });
 
-
-    $("#email").keyup(function() {
-        var email = $(this).val();
-        if(email == "mega91raja@gmail.com") {
-        $("#email_error").html("").hide();
-        } else {
-        $("#email_error").fadeIn("panel").html("Enter Email");
-        }
-    });
-
-    $("#pass").keyup(function() {
-        var pass = $(this).val();
-        if(pass == "9790842304") {
-        $("#pass_error").html("").hide();
-        } else {
-        $("#pass_error").fadeIn("panel").html("Enter Password");
-        }
-    });
-
-        $("#btn").click(function(){
-          const email= $("#email").val();
-          const pass= $("#pass").val();
-          if(email == "" || pass == "") {
-              $("#email_err").html("Please enter Email").addClass("error-msg");
-              $("#pass_err").html("Please enter Password").addClass("error-msg");
-          } else {
-              $("#email_err").html("").removeClass("error-msg");
-              $("#pass_err").html("").removeClass("error-msg");
-          }
-});
-
-/*submitHandler: function(form) {
-          form.submit();
-          }*/
-
-    /*$("#btn").click(function () {
-      //e.preventDefault();
-      var form = $("#login");
-      if (!form.valid()) {
-        alert("invalid");
-        return false;
-      } else {
-      alert("valid");
-        return true;
-      }
-    });*/
-   /* var login = $("#login");
-        if(!login.valid()) {
-            return false;
-        }*//*
-    var login = $("#login");
-    login.submit(function (e) {*/
-
-         /*|| pass != "9790842304" || email != "mega91raja@gmail.com"*/
-  /* var login = $("#login")
-    if(login.valid()) {
-       e.currentTarget.submit();
-        return true;
-    }*/
-
-
-  /*  login.validate({
+    var login = $("#login_form");
+    login.validate({
         rules: {
             email: {
-                required: true,
-                email: true
+            required: true
             },
             password: {
-                required: true
+            required: true
             }
         },
-
         messages: {
-                email: "Please enter Email",
-                password: "Please enter Password"
+            email: "Enter Email",
+            password: "Enter Password"
+        },
+        errorPlacement: function(error, element) {
+        if (element.attr("name") == "email") {
+            $("#email_err_lbl").html(error);
+            $("#email").focus();
+            return false;
+            }
+        if (element.attr("name") == "password") {
+            $("#pass_err_lbl").html(error);
+            $("#password").focus();
+            return false;
+         }
+        },
+        submitHandler: function(form) {
+           form.submit();
         }
-    });*/
     });
-
+});
---
+++ web/WEB-INF/js/signupvalidation.js
@@ -0,0 +1,116 @@
+$(document).ready(function() {
+var signupForm = $("#signup");
+   signupForm.validate({
+      rules: {
+         firstname: {
+            required: true
+         },
+         lastname: {
+            required: true
+         },
+         email: {
+             required: true,
+             email: true,
+         },
+         password: {
+             required: true,
+             minlength: 5,
+             maxlength: 12
+         },
+         conformPassword: {
+             required: true,
+             minlength: 5,
+             maxlength: 12,
+             equalTo: "#pass"
+         },
+         phonenumber: {
+             required: true,
+             number: true,
+             minlength: 10,
+             maxlength: 10
+         },
+         gender: {
+             required: true
+         },
+         birthdate: {
+            required: true,
+            date: true
+         }
+      },
+      messages: {
+            firstname: "Enter first name",
+            lastname: "Enter last name",
+            email: {
+                required: "Enter Email",
+                email: "Please enter a valid email address"
+            },
+            phonenumber: {
+               required: "Enter 10 digits mobile no",
+               number: "Please enter valid phone number",
+               minlength: "Phone number field accept only 10 digits",
+               maxlength: "Phone number field accept only 10 digits"
+            },
+            password: {
+                required:  "Enter Password",
+                minlength: "Password must be at least 5 characters",
+                maxlength: "Password not exceed 12 characters"
+            },
+            conformPassword: {
+                required: "Enter Confirm Password",
+                minlength: "Password must be at least 5 characters",
+                maxlength: "Password not exceed 12 characters",
+                equalTo: "The password and confirm password don't match"
+            },
+            gender: "Select Gender",
+            birthdate: {
+                required: "Enter your DOB",
+                date: "Please enter correct date format"
+            }
+      },
+      errorPlacement: function(error,element) {
+           if (element.attr("name") == "firstname") {
+               $("#fname_err_msg").html(error);
+               return false;
+           }
+           if (element.attr("name") == "lastname") {
+               $("#lname_err_msg").html(error);
+               return false;
+           }
+
+            if (element.attr("name") == "email") {
+               $("#email_err_msg").html(error);
+               return false;
+           }
+            if (element.attr("name") == "phonenumber") {
+                  $("#phone_err_msg").html(error);
+                  return false;
+              }
+            if (element.attr("name") == "password") {
+               $("#pass_err_msg").html(error);
+               return false;
+           }
+            if (element.attr("name") == "conformPassword") {
+               $("#cpass_err_msg").html(error);
+               return false;
+           }
+
+            if (element.attr("name") == "gender") {
+               $("#gender_err_msg").html(error);
+               return false;
+           }
+           else {
+               $("#birthdate_err_msg").html(error);
+               return false;
+           }
+
+           if(element.is(":radio")) {
+              error.appendTo(element.parents(".gender"));
+           } else {
+                  error.insertAfter(element);
+           }
+      },
+      submitHandler: function(form) {
+          form.submit();
+      }
+   });
+ });
