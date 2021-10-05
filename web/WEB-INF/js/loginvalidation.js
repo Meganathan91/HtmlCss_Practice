1d029a079cf268057d2247a6ffc27128c9aeec16
Meganathan91 <mega91raja@gmail.com>
2021-10-05 10:27:38 +0530

    Restricted space for all input fields, Used datepicker for dateofbirth input field and restricted feature date, then successfully login sent username in url.

--- web/WEB-INF/js/loginvalidation.js
+++ web/WEB-INF/js/loginvalidation.js
@@ -1,73 +1,61 @@
-
-$(document).ready(function(){
 var login = $("#login_form");
-    $("#eye").on("click", function() {
-        if($("#pass").val() != ""){
-            $(this).toggleClass("fa-eye fa-eye-slash");
-            $("#pass").attr("type",($(this).hasClass("fa-eye")) ? "text" : "password");
-        }
-    });
-    login.validate({
+$(document).ready(function() {
+    $("#login_form").validate({
         rules: {
             email: {
-                required: true
+                required: true,
+                email: true,
             },
             password: {
-                required: true
+                required: true,
             }
         },
         messages: {
-            email: "Enter Email",
-            password: "Enter Password"
+            email: {
+                required: "Enter Email",
+                email: "Enter valid email",
+
+            },
+            password: {
+                required: "Enter Password",
+            }
         },
         errorPlacement: function(error, element) {
             if (element.attr("name") == "email") {
                 $("#email_err_lbl").html(error);
                 return false;
-                }
+            }
             if (element.attr("name") == "password") {
                 $("#pass_err_lbl").html(error);
                 return false;
             }
         }
     });
-    $("#submit_btn").on("click",function(){
-    var login = $("#login_form");
-        if(!login.valid()){
+    $("#pass").keypress(function(e) {
+            if(e.which === 32) {
+                return false;
+            }
+        });
+    $("#eye").on("click", function() {
+        if ($("#pass").val() != "") {
+            $(this).toggleClass("fa-eye fa-eye-slash");
+            $("#pass").attr("type", ($(this).hasClass("fa-eye")) ? "text" : "password");
+        }
+    });
+    $("#submit_btn").on("click", function() {
+        if (!$("#login_form").valid()) {
             return false;
         }
-        if($("#email").attr("data-email") == $("#email").val() && $("#pass").attr("data-pass") == $("#pass").val()) {
+        if ($("#email").attr("data-email") != $("#email").val() || $("#pass").attr("data-pass") != $("#pass").val()) {
             //alert("Wait until 10 seconds for page redirect to another page...")
-            setTimeout("pageRedirect()", 10000);
-        } else if($("#email").attr("data-email") != $("#email").val() && $("#pass").attr("data-pass") != $("#pass").val()) {
-            $("#email_error, #pass_error").fadeIn("slow");
-            $("#email_error, #pass_error").fadeIn(5000);
-        } else {
-            $("#email_error, #pass_error").fadeOut("slow");
-            $("#email_error, #pass_error").fadeOut(5000);
+            $("#email_password_error").text("Given information is not available");
+            $("#email_password_error").show();
+            setTimeout(function() { $("#email_password_error").hide(); }, 5000);
+            return false;
         }
-
-        if ($("#email").attr("data-email") != $("#email").val()) {
-            $("#email_error").fadeIn("slow");
-            $("#email_error").fadeIn(5000);
-        } else {
-             $("#email_error").fadeOut("slow");
-             $("#email_error").fadeOut(5000);
-        }
-
-        if ($("#pass").attr("data-pass") != $("#pass").val()) {
-            $("#pass_error").fadeIn("slow");
-            $("#pass_error").fadeIn(5000);
-        } else {
-             $("#pass_error").fadeOut("slow");
-             $("#pass_error").fadeOut(5000);
-        }
-
-        function pageRedirect() {
-            window.location.replace = "SignUp.html";
-        }
-
-
+        window.location.href = "SuccessPage.html"+'?UserName=' + $("#email").attr("data-email");
+        //alert($("#email").attr("data-email"));
+        alert("hi");
 
     });
 });
--- web/WEB-INF/js/signupvalidation.js
+++ web/WEB-INF/js/signupvalidation.js
@@ -1,160 +1,158 @@
-$(document).ready(function() {
-
-$("#fname").keypress(function(e){
-    if($(this).val() == ' ') {
-        $("#fname_err_msg").html("Space not allowed").addClass("error");
-    return false;
-    } else {
-        $("#fname_err_msg").html("").removeClass("error");
-    }
-});
-$("#lname").keypress(function(e){
-    if(e.which === 32) {
-    $("#lname_err_msg").html("Space not allowed").addClass("error");
-    return false;
-    }
-});
-var signupForm = $("#signup");
-   signupForm.validate({
-      rules: {
-         firstname: {
-            required: true
-         },
-         lastname: {
-            required: true
-         },
-         email: {
-             required: true,
-             email: true,
-         },
-         password: {
-             required: true,
-             minlength: 5,
-             maxlength: 12
-         },
-         conformPassword: {
-             required: true,
-             minlength: 5,
-             maxlength: 12,
-             equalTo: "#pass"
-         },
-         phonenumber: {
-             required: true,
-             number: true,
-             minlength: 10,
-             maxlength: 10
-         },
-         gender: {
-             required: true
-         },
-         birthdate: {
-            required: true,
-            date: true
-         }
-      },
-      messages: {
-
-            firstname: {
-            required: "Enter first name"
-            },
-            lastname: "Enter last name",
-            email: {
-                required: "Enter Email",
-                email: "Please enter a valid email address"
-            },
-            phonenumber: {
-               required: "Enter 10 digits mobile no",
-               number: "Please enter valid phone number",
-               minlength: "Phone number field accept only 10 digits",
-               maxlength: "Phone number field accept only 10 digits"
-            },
-            password: {
-                required:  "Enter Password",
-                minlength: "Password must be at least 5 characters",
-                maxlength: "Password not exceed 12 characters"
-            },
-            conformPassword: {
-                required: "Enter Confirm Password",
-                minlength: "Password must be at least 5 characters",
-                maxlength: "Password not exceed 12 characters",
-                equalTo: "Password, confirm password don't match"
-            },
-            gender: "Select Gender",
-            birthdate: {
-                required: "Enter your DOB",
-                date: "Please enter correct date format"
-            }
-      },
-      errorPlacement: function(error,element) {
-
-           if (element.attr("name") == "firstname") {
-               $("#fname_err_msg").html(error);
-               return false;
-           }
-           if ($("#fname").val() == ' ') {
-               $("#fname_err_msg").html("Space not allowed");
-               return false;
-           }
-           if (element.attr("name") == "lastname") {
-               $("#lname_err_msg").html(error);
-               return false;
-           }
-
-            if (element.attr("name") == "email") {
-               $("#email_err_msg").html(error);
-               return false;
-           }
-            if (element.attr("name") == "phonenumber") {
-                  $("#phone_err_msg").html(error);
-                  return false;
-              }
-            if (element.attr("name") == "password") {
-               $("#pass_err_msg").html(error);
-               return false;
-           }
-            if (element.attr("name") == "conformPassword") {
-               $("#cpass_err_msg").html(error);
-               return false;
-           }
-
-            if (element.attr("name") == "gender") {
-               $("#gender_err_msg").html(error);
-               return false;
-           }
-           else {
-               $("#birthdate_err_msg").html(error);
-               return false;
-           }
-
-           if(element.is(":radio")) {
-              error.appendTo(element.parents(".gender"));
-           } else {
-                  error.insertAfter(element);
-           }
-      },
-      submitHandler: function(form) {
-      form.submit();
-        var fname = $('input[name="firstname"]').val();
-        var lname = $('input[name="lastname"]').val();
-        var email = $('input[name="email"]').val();
-        var phone = $('input[name="phonenumber"]').val();
-        var password = $('input[name="password"]').val();
-        var conformPassword = $("input[name='conformPassword']").val();
-        var gender = $("input[name='gender']:checked").val();
-        var birthdate = $('input[name="birthdate"]').val();
-      console.log(fname +"\n"+ lname +"\n"+ email +"\n"+ phone +"\n"+ password +"\n"+ conformPassword +"\n"+ gender +"\n"+ birthdate);
-      }
-      });
-     /*$("#signup").submit(function(event){
-        //event.preventDefault();
-        var name = $("input[type='text']",this).val();
-        var email = $("#email",this).val();*//**//*
-        var fieldValuePairs = $("#signup").serializeArray();
-        $.each(fieldValuePairs, function(index, fieldValuePair) {
-            console.log("Item " + index + " is [" + fieldValuePair.name + "," + fieldValuePair.value + "]");
+    $(document).ready(function() {
+    $('input').keypress(function(e){
+        if(e.which === 32) {
+            $("#error_msg").text("Space is not Allowed");
+                   $("#error_msg").show();
+                   setTimeout(function() { $("#error_msg").hide(); }, 500);
+                   return false;
+        }
+    });
+    $(function(){
+        $("#datepicker").datepicker({
+            dateFormat: "yy-MM-dd",
+            maxDate: new Date()
         });
-        var name = $("#fname").val();
-        console.log(name);
-        });*/
+    });
+    var signupForm = $("#signup_form");
+       signupForm.validate({
+          rules: {
+             firstname: {
+                required: true,
+                minlength: 3,
+                maxlength: 30
+             },
+             lastname: {
+                required: true,
+                minlength: 3,
+                maxlength: 30
+             },
+             email: {
+                 required: true,
+                 email: true
+             },
+             password: {
+                 required: true,
+                 minlength: 5,
+                 maxlength: 12
+             },
+             conformPassword: {
+                 required: true,
+                 minlength: 5,
+                 maxlength: 12,
+                 equalTo: "#pass"
+             },
+             phonenumber: {
+                 required: true,
+                 number: true,
+                 minlength: 10,
+                 maxlength: 10
+             },
+             gender: {
+                 required: true
+             },
+             birthdate: {
+                required: true,
+             }
+          },
+          messages: {
+                firstname: {
+                required: "Enter your first name",
+                minlength: "Enter at least 3 characters",
+                maxlength: "First name too long more than (30) characters"
 
- });
+                },
+                lastname: {
+                required: "Enter last name"
+                },
+                email: {
+                    required: "Enter Email",
+                    email: "Please enter a valid email address"
+                },
+                phonenumber: {
+                   required: "Enter 10 digits mobile no",
+                   number: "Please enter valid phone number",
+                   minlength: "Phone number field accept only 10 digits",
+                   maxlength: "Phone number field accept only 10 digits"
+                },
+                password: {
+                    required:  "Enter Password",
+                    minlength: "Password must be at least 5 characters",
+                    maxlength: "Password not exceed 12 characters"
+                },
+                conformPassword: {
+                    required: "Enter Confirm Password",
+                    minlength: "Password must be at least 5 characters",
+                    maxlength: "Password not exceed 12 characters",
+                    equalTo: "Password, confirm password don't match"
+                },
+                gender: "Select Gender",
+                birthdate: {
+                    required: "Enter your DOB"
+                }
+          },
+          errorPlacement: function(error,element) {
+
+               if (element.attr("name") == "firstname") {
+                   $("#fname_err_msg").html(error);
+                   return false;
+               }
+              /* if ($("#fname").val() == ' ') {
+                   $("#fname_err_msg").html("Space not allowed");
+                   return false;
+               }*/
+               if (element.attr("name") == "lastname") {
+                   $("#lname_err_msg").html(error);
+                   return false;
+               }
+
+                if (element.attr("name") == "email") {
+                   $("#email_err_msg").html(error);
+                   return false;
+               }
+                if (element.attr("name") == "phonenumber") {
+                      $("#phone_err_msg").html(error);
+                      return false;
+                  }
+                if (element.attr("name") == "password") {
+                   $("#pass_err_msg").html(error);
+                   return false;
+               }
+                if (element.attr("name") == "conformPassword") {
+                   $("#cpass_err_msg").html(error);
+                   return false;
+               }
+
+                if (element.attr("name") == "gender") {
+                   $("#gender_err_msg").html(error);
+                   return false;
+               }
+               else {
+                   $("#birthdate_err_msg").html(error);
+                   return false;
+               }
+
+               if(element.is(":radio")) {
+                  error.appendTo(element.parents(".gender"));
+               } else {
+                      error.insertAfter(element);
+               }
+          }/*,
+          submitHandler: function(form) {
+          form.submit();
+            var fname = $('input[name="firstname"]').val();
+            var lname = $('input[name="lastname"]').val();
+            var email = $('input[name="email"]').val();
+            var phone = $('input[name="phonenumber"]').val();
+            var password = $('input[name="password"]').val();
+            var conformPassword = $("input[name='conformPassword']").val();
+            var gender = $("input[name='gender']:checked").val();
+            var birthdate = $('input[name="birthdate"]').val();
+          console.log(fname +"\n"+ lname +"\n"+ email +"\n"+ phone +"\n"+ password +"\n"+ conformPassword +"\n"+ gender +"\n"+ birthdate);
+          }*/
+          });
+          $("#submit_signup_btn").on("click", function() {
+                  if (!$("#signup_form").valid()) {
+                      return false;
+                  }
+          });
+    });
